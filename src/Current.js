a99a3ef6b508bb3cd8b0c592bc2d06f66c559a8a
Joakim J. Rapp <joakim.rapp@viaplay.com>
2017-08-07 15:44:26 +0200

    first commit

---
+++ src/Current.js
@@ -0,0 +1,82 @@
+module.exports = class Current {
+	constructor( { resolve, reject, absolutepath, size = 0 } = {} ) {
+		this.context = {
+			resolve,
+			reject,
+			absolutepath,
+			size,
+			processedbytes: 0,
+			open: false,
+			paused: false,
+			processed: 0,
+			pending: 0,
+			resolved: true,
+			on: {
+				close: () => {},
+				line: () => {}
+			}
+		};
+	}
+	get on() {
+		const that = this;
+		const context = that.context;
+		if( !context.readlineInterface ) {
+			context.readlineInterface = require( 'readline' ).createInterface( { input: require( 'fs' ).createReadStream( context.absolutepath ) } );
+			context.open = true;
+			context.resolved = false;
+			context.readlineInterface.on( 'close', () => {
+				context.open = false;
+				if( !context.pending )
+					context.on.close( that );
+			} );
+			context.readlineInterface.on( 'line', ( line ) => {
+				context.pending++;
+				Promise.resolve( line )
+					.then( context.on.line )
+					.then( ( processed ) => {
+						if( processed )
+							context.processed++;
+						context.pending--;
+						context.processedbytes += line.length + 1;
+						if( ( !context.pending ) && ( !context.open ) )
+							context.on.close( that );
+					} )
+					.catch( context.reject );
+			} );
+		}
+		return {
+			close: ( onFulfilled ) => ( ( context.on.close = onFulfilled ), that ),
+			line: ( listener ) => ( ( context.on.line = listener ), that )
+		};
+	}
+	pause() {
+		this.context.paused = true;
+		this.context.readlineInterface.pause();
+	};
+	resume() {
+		this.context.paused = false;
+		this.context.readlineInterface.resume();
+	};
+	resolve() {
+		this.context.resolved = true;
+		const { resolve, absolutepath, processed } = this.context;
+		return Promise.resolve( { absolutepath, processed } )
+			.then( resolve );
+	}
+	get resolved() { return this.context.resolved; }
+	get absolutepath() { return this.context.absolutepath; }
+	get size() { return this.context.size; }
+	get processedbytes() { return this.context.processedbytes; }
+	get paused() { return this.context.paused; }
+	get processed() { return this.context.processed; }
+	get pending() { return this.context.pending; }
+	get status() {
+		return {
+			absolutepath: this.context.absolutepath,
+			filename: this.context.absolutepath ? require( 'path' ).basename( this.context.absolutepath ) : undefined,
+			open: this.context.open,
+			pending: this.context.pending,
+			paused: this.context.paused
+		};
+	}
+};
---
+++ src/index.js
@@ -0,0 +1,57 @@
+module.exports = ( maxThroughput ) => {
+	const promiseThrottler = require( '@jrapp/promise-throttler' );
+	const Current = require( './Current.js' );
+	const throttler = maxThroughput > 0 ? promiseThrottler( maxThroughput, 1000 ) : promiseThrottler( 1, 1000 ).disable();
+	let totalbytes = 0;
+	let processedbytes = 0;
+	const queue = [];
+	let current = new Current();
+	let filter = () => true, map = ( item ) => item, each = () => {}, error = ( ( { line, error } ) => Promise.reject( error ) );
+	const run = () => {
+		if( ( current.resolved ) && ( !!queue.length ) ) {
+			current = new Current( queue.shift() )
+				.on.line( ( line ) => Promise.resolve( line )
+			 		.then( filter )
+				 	.then( ( valid ) => {
+						if( valid ) {
+							if( !current.paused && ( throttler.queued > throttler.maxThroughput ) )
+								current.pause();
+							return Promise.resolve( line )
+								.then( throttler.throttle )
+								.then( map )
+								.then( each )
+								.then( () => {
+									if( current.paused && ( throttler.queued < throttler.maxThroughput ) )
+										current.resume();
+									return true;
+								} );
+						}
+						else
+							return false;
+					} )
+					.catch( ( err ) => error( { error: err, line } ) ) )
+				.on.close( () => {
+					processedbytes += current.size;
+					current.resolve().then( run );
+				} );
+		}
+	};
+	const { fs } = require( '@jrapp/callbacks-to-promises' );
+	const pub = {
+		process: ( absolutepath ) => new Promise( ( resolve, reject ) => fs.stat( absolutepath )
+			.then( ( { size } ) => ( ( totalbytes += size ), queue.push( { absolutepath, resolve, reject, size } ), run() ) ) ),
+		filter: ( fn ) => ( ( filter = fn ), pub ),
+		map: ( fn ) => ( ( map = fn ), pub ),
+		each: ( fn ) => ( ( each = fn ), pub ),
+		error: ( fn ) => ( ( error = fn ), pub ),
+		status: () => ( {
+			queued: queue.length,
+			completed: ( processedbytes + current.processedbytes ) / totalbytes,
+			throughput: throttler.throughput,
+			current: current.status
+		} ),
+		maxThroughput: ( maxThroughput ) =>
+			( ( throttler.enable().maxThroughput = maxThroughput ), pub )
+	};
+	return pub;
+};
